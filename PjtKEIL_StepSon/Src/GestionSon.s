83a7192de10b5d469ab15e00e9f6b224b220e79f
Paul Gervais <pgervais@insat.insa-toulouse.fr>
2021-04-06 18:11:39 +0200

    modif son (pas fonctionnel)

--- PjtKEIL_StepSon/Src/GestionSon.s
+++ PjtKEIL_StepSon/Src/GestionSon.s
@@ -1,7 +1,6 @@
 	PRESERVE8
 	THUMB   
 		
-
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
 	area    mesdata,data,readonly
@@ -10,21 +9,42 @@
 ;Section RAM (read write):
 	area    maram,data,readwrite
 		
-
+SortieSon DCD 0
+AdressSon DCD 0
 	
 ; ===============================================================================================
-	
-
-
 		
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
-; écrire le code ici		
+; écrire le code ici	
+	export CallbackSon
+	export SortieSon
+	
+    import Son ;Adresse du tableau de sons
 
-
-
-
-
+CallbackSon
+	
+	ldr r1, =AdressSon
+	ldr r3, =SortieSon
+	ldr r0, [r1] ; r1 adresse du son, r0 valeur
+	
+	cmp r0, #0 ; Si adresse son vaut 0
+	bne lire_son
+	ldr r0, =Son
+	b lire_son
 		
-		
+lire_son
+
+	ldrsh r2,[r0] ; half : lis sur 16 bit
+	adds r2, #32768
+	mov r4, #719
+	mul r2, r4
+	mov r4, #65535
+	sdiv r2, r4
+	str r2, [r3] ; stock le son dans SortieSon
+	add r0, #2 ; incrémente de 16 bits
+	str r0, [r1] ; stock adresse son
+
+	bx lr
+	
 	END	

ad451eac53263ef864dd6599aafee1fb7f6b4d89
iSpeX <75473395+iSpeX1@users.noreply.github.com>
2021-04-13 16:39:24 +0200

    Add files via upload

--- PjtKEIL_StepSon/Src/GestionSon.s
+++ PjtKEIL_StepSon/Src/GestionSon.s
@@ -39,7 +39,7 @@ lire_son
 	adds r2, #32768
 	mov r4, #719
 	mul r2, r4
-	mov r4, #65535
+	mov r4, #65536
 	sdiv r2, r4
 	str r2, [r3] ; stock le son dans SortieSon
 	add r0, #2 ; incrémente de 16 bits
